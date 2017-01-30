2eb3d9263124defa5350be0c1a5973181d955920
Ariel De Ocampo <arielmakestuff@gmail.com>
2017-01-30 14:14:16 -0500

    Create generator sub-package

---
+++ loadlimit/generator/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+# loadlimit/generator/__init__.py
+# Copyright (C) 2017 authors and contributors (see AUTHORS file)
+#
+# This module is released under the MIT License.
+
+"""loadlimit.generator package"""
